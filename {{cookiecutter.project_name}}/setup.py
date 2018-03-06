04f32c0b46fbadce4bc19d86c7488a6e8569042a
ktpr1223 <michael.pero1223@gmail.com>
2018-03-07 02:44:25 +0900

    setup.pyに依存package関連の設定を追加

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -3,9 +3,16 @@ from setuptools import setup
 project_name = '{{cookiecutter.project_name}}'
 author = '{{cookiecutter.author_name}}'
 
+install_requires = ['PyYAML']
+extras_require = {
+    'test': ['pytest', 'flake8', 'pytest-cov'],
+}
+
 setup(
     name=project_name,
     version='0.1',
     author=author,
     # packagesは指定する必要あり
-    packages=['{{cookiecutter.project_name}}'])
+    packages=['{{cookiecutter.project_name}}'],
+    install_requires=install_requires,
+    extras_require=extras_require)
